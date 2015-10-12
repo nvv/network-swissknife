d4b67c53842244c6b7ec940cf9445a2507cf2734
vladnamashko <vnamashko@gmail.com>
2015-10-13 00:51:50 +0300

    init version

--- app/src/main/java/com/easymobile/lan/scanner/main/HostScan.java
+++
@@ -1,9 +0,0 @@
-package com.easymobile.lan.scanner.main;
-
-public class HostScan {
-    static {
-        System.loadLibrary("host_scan");
-    }
-
-    public native String gethostname(String paramString);
-}
